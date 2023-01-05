b3ce01587c5f4d7579bd9d708ec4beac30b8488c
domieee <99103235+domieee@users.noreply.github.com>
2023-01-06 00:01:53 +0100

    added js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,22 @@
-document.addEventListener()
+let toggleDarkModeButton = document.getElementById("header__toggle-dark-mode");
+let body = document.querySelector("body");
+const n0 = 0;
+const n1 = 1;
+let state = 0;
+
+function toggleDarkMode() {
+    if (state === 0) {
+        body.style.backgroundColor = "rgb(0,0,0)";
+        body.style.color = "rgb(255,255,255)";
+        state = 1;
+    } else {
+        body.style.backgroundColor = "rgb(255,255,255)";
+        body.style.color = "rgb(0,0,0)";
+        state = 0;
+    }
+
+}
+
+
+toggleDarkModeButton.addEventListener("click", toggleDarkMode);
+
